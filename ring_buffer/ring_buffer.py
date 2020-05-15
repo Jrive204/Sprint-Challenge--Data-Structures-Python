05c1e6aa0aa5a3a0190b73604da322caa238cb04
Jose Rivera <jrivera6869@gmail.com>
2020-05-15 12:14:51 -0400

    sll

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -30,7 +30,7 @@ class RingBuffer:
             new_node = Node(item)
             current = self.head
             self.head = new_node
-            # print(self.capacity, current.value)
+            print(self.capacity, current.value, "????")
             new_node.set_next(current)
 
             for i in range(0, self.capacity - 2):
@@ -39,8 +39,8 @@ class RingBuffer:
             current.set_next(None)
 
             self.head = new_node
+            print(self.extra, "I MADE IT")
 
-            # print(self.extra, "I MADE IT")
             return
         elif self.size < self.capacity:
             self.size += 1
@@ -60,6 +60,7 @@ class RingBuffer:
             current = self.head
             print(self.extra, current.value, "EXTRA")
             if self.extra <= 1:
+                print("SHOULD ONLY BE 1")
 
                 new_node.set_next(current.get_next())
                 self.head = new_node
@@ -72,33 +73,43 @@ class RingBuffer:
                 return
 
             else:
-                # self.extra += 1
-                # print(self.extra, "EXTRA")
-
-                new_node.set_next(current)
-                self.head = new_node
+                # self.head = new_node
+                # self.head.set_next(None)
                 print(
                     range(self.extra),
                     self.head.value,
+                    self.head.get_next().value,
+                    new_node.value,
+                    current.get_next().get_next().value,
+                    # new_node.get_next().value,
+                    # new_node.get_next().get_next().value,
                     "IN ELSE "
                 )
+
+                # self.head.set_next(current.get_next())
                 a = self.head
+                for num in range(self.extra - 2):
+                    current = current.get_next()
+                    # print(current.value, "IN FOR CURRENT")
                 for num in range(self.extra):
                     a = a.get_next()
-                    # print(a.get_value(), "inside Loop")
+                    print("LOOP")
+                # print(a.get_value(), current.get_next().value, "IN FOR")
+                new_node.set_next(a)
+                current.set_next(new_node)
+                # self.head.set_next(current)
 
-                while current.get_next() is not None:
-                    if current.get_next().get_value() == a.get_value():
-                        d = current.get_next().get_next()
-                        current.set_next(d)
-                        # print(current.get_value(), d, "CHECKING 2")
-                    else:
-                        current = current.get_next()
-                        # print(
-                        #     current.get_value(),
-                        #     # current.get_next().get_value(),
-                        #     # current.get_next().get_next().get_value(),
-                        #     "Curious")
+                print(self.head.get_next().value, "HAIL MARY")
+                if self.capacity == self.extra:
+                    self.extra = 0
+
+                # while current.get_next() is not None:
+                #     if current.get_next().get_value() == a.get_value():
+                #         d = current.get_next().get_next()
+                #         current.set_next(d)
+                #         print(current.get_value(), "IN WHILE")
+                #     else:
+                #         current = current.get_next()
 
                 # self.head.set_next(new_node)
                 # 11 needs to kill 101 and keep 4   12
@@ -137,8 +148,11 @@ if __name__ == "__main__":
     print(l.get(), "Length2:", l.__len__())
     l.append('i')
     print(l.get(), "Length2:", l.__len__())
+    l.append('j')
+    print(l.get(), "Length2:", l.__len__())
+    l.append("k")
+    print(l.get(), "Length2:", l.__len__())
 
-    # l.append(4)
     # l.append(11)
     # l.append(16)
 
